11dd065798c1a4f951691d53d649416fe673e80a
szabototo89 <szabototo89@gmail.com>
2013-10-17 15:33:14 +0200

    NewGameContext változtatások

--- Catan/Catan/MainWindow.xaml.cs
+++ Catan/Catan/MainWindow.xaml.cs
@@ -18,27 +18,27 @@ using Catan.ViewModel;
 
 namespace Catan
 {
-	public partial class MainWindow : Window
-	{
-		public MainWindow()
-		{
-			InitializeComponent();
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
 
-			var context = new GameTableContext(7, new WPFWindowService(this));
+            var context = new GameTableContext(7, new WPFWindowService(this));
 
-			/*context.GameCells = new List<GameCellContext>()
-				                    {
-										new GameCellContext(context, new Hexagon(10, Material.Iron)) { Value = 1},
-										new GameCellContext(context, new Hexagon(10, Material.Wheat)) { Value = 2},
-										new GameCellContext(context, new Hexagon(10, Material.Wood)) { Value = 3},
-										new GameCellContext(context, new Hexagon(10, Material.Wool)) { Value = 4},
-				                    };*/
+            /*context.GameCells = new List<GameCellContext>()
+                                    {
+                                        new GameCellContext(context, new Hexagon(10, Material.Iron)) { Value = 1},
+                                        new GameCellContext(context, new Hexagon(10, Material.Wheat)) { Value = 2},
+                                        new GameCellContext(context, new Hexagon(10, Material.Wood)) { Value = 3},
+                                        new GameCellContext(context, new Hexagon(10, Material.Wool)) { Value = 4},
+                                    };*/
 
-			context.GameCells = new List<GameCellContext>();
+            context.GameCells = new List<GameCellContext>();
 
-			var random = new Random();
+            var random = new Random();
 
-			var materials = new[]
+            var materials = new[]
 				                {
 									Material.Wood,
 									Material.Wool, 
@@ -47,25 +47,22 @@ namespace Catan
 									Material.Iron
 				                };
 
-            for (var j = 0; j < 7; ++j)
-            {
-                for (var i = 0; i < 7 - Math.Abs(3 - j); ++i)
-                {
-                    Hexagon h = new Hexagon(10, materials[random.Next(0, materials.Length)], new Hexid(j,i) );
+            for (var j = 0; j < 7; ++j) {
+                for (var i = 0; i < 7 - Math.Abs(3 - j); ++i) {
+                    Hexagon h = new Hexagon(10, materials[random.Next(0, materials.Length)], new Hexid(j, i));
                     (context.GameCells as List<GameCellContext>)
-						.Add(new GameCellContext(context,
-							 h) { Value = random.Next(2, 13) });
+                        .Add(new GameCellContext(context, h) { Value = random.Next(2, 13) });
                     GameController.Instance.Hexagons.Add(h);
                 }
             }
 
             GameController.Instance.SetAllNeighbours();
-			NewGameWindow newGameWindow = new NewGameWindow();
-			var newGameContext = new NewGameContext(context, new Size(7, 7), new WPFWindowService(newGameWindow));
-			newGameWindow.DataContext = newGameContext;
-			newGameWindow.ShowDialog();
+            NewGameWindow newGameWindow = new NewGameWindow();
+            var newGameContext = new NewGameContext(context, new Size(7, 7), new WPFWindowService(newGameWindow));
+            newGameWindow.DataContext = newGameContext;
+            newGameWindow.ShowDialog();
 
-			DataContext = context;
-		}
-	}
+            DataContext = context;
+        }
+    }
 }
--- Catan/Catan/ViewModel/NewGameContext.cs
+++ Catan/Catan/ViewModel/NewGameContext.cs
@@ -24,6 +24,8 @@ namespace Catan.ViewModel
         /// </summary>
         private List<string> _SelectedPlayers;
 
+        private int _TableSize;
+
         /// <summary>
         /// A játéktábla nézetmodellje
         /// </summary>
@@ -91,6 +93,17 @@ namespace Catan.ViewModel
 				};
             }
         }
+
+        public int TableSize
+        {
+            get { return _TableSize; }
+            set
+            {
+                _TableSize = value;
+                OnPropertyChanged(() => TableSize);
+            }
+        }
+
 
         /// <summary>
         /// Aktív színek állítását figyelő metódus
