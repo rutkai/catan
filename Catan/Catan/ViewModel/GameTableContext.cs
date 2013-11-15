c06ec5eb3eeee454bded6951b6e48fc390fed91f
szabototo89 <szabototo89@gmail.com>
2013-11-15 09:09:39 +0100

    Fixed ProgressBar

--- Catan/Catan/ViewModel/GameTableContext.cs
+++ Catan/Catan/ViewModel/GameTableContext.cs
@@ -14,229 +14,240 @@ using Catan.View;
 
 namespace Catan.ViewModel
 {
-	/// <summary>
-	/// GameTable nézetmodellje
-	/// </summary>
-	public class GameTableContext : ViewModelBase
-	{
-		private ActionCommand _ShowTradeWindowCommand;
-		private ActionCommand _ShowNewGameWindowCommand;
-		private IEnumerable<GameCellContext> _GameCells;
-		private GameCellContext _SelectedGameCell;
-		private Size _TableSize;
-		private DelegateCommand<GameCellContext> _SelectGameCellCommand;
-		private ActionCommand _StepCommand;
-		private ImageSource _BackgroundImage;
+    /// <summary>
+    /// GameTable nézetmodellje
+    /// </summary>
+    public class GameTableContext : ViewModelBase
+    {
+        private ActionCommand _ShowTradeWindowCommand;
+        private ActionCommand _ShowNewGameWindowCommand;
+        private IEnumerable<GameCellContext> _GameCells;
+        private GameCellContext _SelectedGameCell;
+        private Size _TableSize;
+        private DelegateCommand<GameCellContext> _SelectGameCellCommand;
+        private ActionCommand _StepCommand;
+        private ImageSource _BackgroundImage;
 
-		public IFace.IWindowService WindowService { get; set; }
+        public IFace.IWindowService WindowService { get; set; }
 
-		public GameTableContext(IFace.IWindowService service)
-			: this(5, 5, service)
-		{
-		}
+        public GameTableContext(IFace.IWindowService service)
+            : this(5, 5, service)
+        {
+        }
 
-		public GameTableContext(uint size, IWindowService service)
-			: this(size, size, service)
-		{
+        public GameTableContext(uint size, IWindowService service)
+            : this(size, size, service)
+        {
 
-		}
+        }
 
-		/// <summary>
-		/// Konstruktor, ami megkapja paraméteréül a tábla méretét
-		/// </summary>
-		private GameTableContext(uint width, uint height, IFace.IWindowService service)
-		{
-			//_BackgroundImage = new BitmapImage(new Uri("Images//sea_field.png"));
-			_TableSize = new Size(width, height);
-			GameController.Instance.Init(width, new[] { new Player("Gipsz Jakab", PlayerColor.Blue),
+        /// <summary>
+        /// Konstruktor, ami megkapja paraméteréül a tábla méretét
+        /// </summary>
+        private GameTableContext(uint width, uint height, IFace.IWindowService service)
+        {
+            //_BackgroundImage = new BitmapImage(new Uri("Images//sea_field.png"));
+            _TableSize = new Size(width, height);
+            GameController.Instance.Init(width, new[] { new Player("Gipsz Jakab", PlayerColor.Blue),
 														new Player("Játékos 2", PlayerColor.Red) });
 
-			if (service == null)
-				throw new ArgumentNullException("service");
-			WindowService = service;
-		}
+            if (service == null)
+                throw new ArgumentNullException("service");
+            WindowService = service;
+        }
 
-		/// <summary>
-		/// Aktuális játékos
-		/// </summary>
-		public Player CurrentPlayer
-		{
-			get { return GameController.Instance.CurrentPlayer; }
-		}
+        /// <summary>
+        /// Aktuális játékos
+        /// </summary>
+        public Player CurrentPlayer
+        {
+            get { return GameController.Instance.CurrentPlayer; }
+        }
 
-		/// <summary>
-		/// Játékosokat reprezentáló tömb
-		/// </summary>
-		public IEnumerable<Player> Players
-		{
-			get { return GameController.Instance.Players; }
-		}
+        /// <summary>
+        /// Játékosokat reprezentáló tömb
+        /// </summary>
+        public IEnumerable<Player> Players
+        {
+            get { return GameController.Instance.Players; }
+        }
 
-		/// <summary>
-		/// Aktuálisan kijelölt játékcella
-		/// </summary>
-		public GameCellContext SelectedGameCell
-		{
-			get { return _SelectedGameCell; }
-			set
-			{
-				_SelectedGameCell = value;
-				OnPropertyChanged(() => SelectedGameCell);
-			}
-		}
+        /// <summary>
+        /// Aktuálisan kijelölt játékcella
+        /// </summary>
+        public GameCellContext SelectedGameCell
+        {
+            get { return _SelectedGameCell; }
+            set
+            {
+                _SelectedGameCell = value;
+                OnPropertyChanged(() => SelectedGameCell);
+            }
+        }
 
-		/// <summary>
-		/// Játéktáblán lévő cellák
-		/// </summary>
-		public IEnumerable<GameCellContext> GameCells
-		{
-			get { return _GameCells; }
-			set
-			{
-				_GameCells = value;
-				OnPropertyChanged(() => GameCells);
-			}
-		}
+        /// <summary>
+        /// Játéktáblán lévő cellák
+        /// </summary>
+        public IEnumerable<GameCellContext> GameCells
+        {
+            get { return _GameCells; }
+            set
+            {
+                _GameCells = value;
+                OnPropertyChanged(() => GameCells);
+            }
+        }
 
-		/// <summary>
-		/// Tábla mérete
-		/// </summary>
-		public Size TableSize
-		{
-			get { return _TableSize; }
-			set
-			{
-				_TableSize = value;
-				OnPropertyChanged(() => TableSize);
-			}
-		}
+        /// <summary>
+        /// Tábla mérete
+        /// </summary>
+        public Size TableSize
+        {
+            get { return _TableSize; }
+            set
+            {
+                _TableSize = value;
+                OnPropertyChanged(() => TableSize);
+            }
+        }
 
-		/// <summary>
-		/// Aktuális játékcella kijelölése
-		/// </summary>
-		public DelegateCommand<GameCellContext> SelectGameCellCommand
-		{
-			get
-			{
-				return Lazy.Init(ref _SelectGameCellCommand,
-								 () => new DelegateCommand<GameCellContext>(
-									 value =>
-									 {
-										 if (SelectedGameCell != null)
-										 {
-											 SelectedGameCell.BuildRoadMode = false;
-											 SelectedGameCell.BuildTownMode = false;
-										 }
-										 SelectedGameCell = value;
-									 }));
-			}
-		}
+        /// <summary>
+        /// Aktuális játékcella kijelölése
+        /// </summary>
+        public DelegateCommand<GameCellContext> SelectGameCellCommand
+        {
+            get
+            {
+                return Lazy.Init(ref _SelectGameCellCommand,
+                                 () => new DelegateCommand<GameCellContext>(
+                                     value => {
+                                         if (SelectedGameCell != null) {
+                                             SelectedGameCell.BuildRoadMode = false;
+                                             SelectedGameCell.BuildTownMode = false;
+                                         }
+                                         SelectedGameCell = value;
+                                     }));
+            }
+        }
 
-		/// <summary>
-		/// Játékban való lépés
-		/// </summary>
-		public ActionCommand StepCommand
-		{
-			get
-			{
-				return Lazy.Init(ref _StepCommand, () => new ActionCommand(
-					() =>
-					{
-                        try
-                        {
+        /// <summary>
+        /// Játékban való lépés
+        /// </summary>
+        public ActionCommand StepCommand
+        {
+            get
+            {
+                return Lazy.Init(ref _StepCommand, () => new ActionCommand(
+                    () => {
+                        try {
                             GameController.Instance.Step();
                         }
-                        catch (Exception e)
-                        {
+                        catch (Exception e) {
                             MessageBox.Show(e.Message);
                             WindowService.Close();
                         }
-						OnPropertyChanged(() => CurrentPlayer, 
-										  () => TradeContext, 
-										  () => DiceResult, 
-										  () => Materials);
-					}));
-			}
-		}
+                        OnPropertyChanged(() => CurrentPlayer,
+                                          () => TradeContext,
+                                          () => DiceResult,
+                                          () => Materials);
+                    }));
+            }
+        }
 
-		public TradeContext TradeContext
-		{
-			get
-			{
-				return new TradeContext(this, CurrentPlayer);
-			}
-		}
+        public TradeContext TradeContext
+        {
+            get
+            {
+                return new TradeContext(this, CurrentPlayer);
+            }
+        }
 
-		public ImageSource BackgroundImage
-		{
-			get { return _BackgroundImage; }
-			set
-			{
-				_BackgroundImage = value;
-				OnPropertyChanged(() => BackgroundImage);
-			}
-		}
+        public ImageSource BackgroundImage
+        {
+            get { return _BackgroundImage; }
+            set
+            {
+                _BackgroundImage = value;
+                OnPropertyChanged(() => BackgroundImage);
+            }
+        }
 
-		public ActionCommand ShowTradeWindowCommand
-		{
-			get
-			{
-				return Lazy.Init(ref _ShowTradeWindowCommand,
-					() => new ActionCommand(() =>
-						{
-							var tradeControl = new TradeControl();
-							tradeControl.SetBinding(FrameworkElement.DataContextProperty,
-								new Binding("TradeContext")
-									{
-										Source = this
-									});
-							tradeControl.ShowDialog();
-						}));
-			}
-		}
+        public ActionCommand ShowTradeWindowCommand
+        {
+            get
+            {
+                return Lazy.Init(ref _ShowTradeWindowCommand,
+                    () => new ActionCommand(() => {
+                        var tradeControl = new TradeControl();
+                        tradeControl.SetBinding(FrameworkElement.DataContextProperty,
+                            new Binding("TradeContext") {
+                                Source = this
+                            });
+                        tradeControl.ShowDialog();
+                    }));
+            }
+        }
 
-		public ActionCommand ShowNewGameWindowCommand
-		{
-			get
-			{
-				return Lazy.Init(ref _ShowNewGameWindowCommand,
-					() => new ActionCommand(() =>
-					{
-						var newGameControl = new NewGameWindow();
-						newGameControl.DataContext = new NewGameContext(this, _TableSize, new WPFWindowService(newGameControl));
-						newGameControl.ShowDialog();
-					}));
-			}
-		}
+        public ActionCommand ShowNewGameWindowCommand
+        {
+            get
+            {
+                return Lazy.Init(ref _ShowNewGameWindowCommand,
+                    () => new ActionCommand(() => {
+                        var newGameControl = new NewGameWindow();
+                        newGameControl.DataContext = new NewGameContext(this, _TableSize, new WPFWindowService(newGameControl));
+                        newGameControl.ShowDialog();
+                    }));
+            }
+        }
 
-		public override void Refresh()
-		{
-			foreach (var cell in GameCells)
-				if (cell != null)
-					cell.Refresh();
+        public override void Refresh()
+        {
+            foreach (var cell in GameCells)
+                if (cell != null)
+                    cell.Refresh();
 
-			OnPropertyChanged(() => Materials);
-		}
+            OnPropertyChanged(() => Materials);
+        }
 
-		public int[] DiceResult
-		{
-			get
-			{
-				return new[] { GameController.Instance.Dobas1, GameController.Instance.Dobas2 };
-			}
-		}
+        public int[] DiceResult
+        {
+            get
+            {
+                return new[] { GameController.Instance.Dobas1, GameController.Instance.Dobas2 };
+            }
+        }
 
-		/// <summary>
-		/// Visszatér az aktuális játékos nyersanyagaival
-		/// </summary>
-		public List<Tuple<Material, int>> Materials
-		{
-			get
-			{
-				return CurrentPlayer.Materials
-									.Select(item => Tuple.Create(item.Key, item.Value))
-									.ToList();
-			}
-		}
-	}
+        /// <summary>
+        /// Visszatér az aktuális játékos nyersanyagaival
+        /// </summary>
+        /*public List<Tuple<Material, int>> Materials
+        {
+            get
+            {
+                return CurrentPlayer.Materials
+                                    .Select(item => Tuple.Create(item.Key, item.Value))
+                                    .ToList();
+            }
+        }*/
+
+        public int MaximumMaterialQuantity
+        {
+            get { return 3; }
+        }
+
+        public List<object> Materials
+        {   
+            get
+            {
+                return CurrentPlayer.Materials
+                                    .Select(item => new {
+                                        Material = item.Key,
+                                        Quantity = item.Value,
+                                        IsFull = item.Value > MaximumMaterialQuantity
+                                    })
+                                    .ToList<object>();
+            }
+        }
+    }
 }
--- Catan/Catan/ViewModel/TradeContext.cs
+++ Catan/Catan/ViewModel/TradeContext.cs
@@ -8,112 +8,107 @@ using Catan.Model;
 
 namespace Catan.ViewModel
 {
-	/// <summary>
-	/// Kereskedésért felelős nézetmodell
-	/// </summary>
-	public class TradeContext : ViewModelBase
-	{
-		private DelegateCommand<object> _AddCommand;
-		private DelegateCommand<Material> _DeleteCommand;
-		public GameTableContext GameTableContext;
-		protected Player Player { get; set; }
+    /// <summary>
+    /// Kereskedésért felelős nézetmodell
+    /// </summary>
+    public class TradeContext : ViewModelBase
+    {
+        private DelegateCommand<object> _AddCommand;
+        private DelegateCommand<Material> _DeleteCommand;
+        public GameTableContext GameTableContext;
+        protected Player Player { get; set; }
 
-		public TradeContext(GameTableContext context, Player player)
-		{
-			GameTableContext = context;
-			Player = player;
-		}
+        public TradeContext(GameTableContext context, Player player)
+        {
+            GameTableContext = context;
+            Player = player;
+        }
 
-		/// <summary>
-		/// Elérhető termékek, amik még nincsenek meghirdetve
-		/// </summary>
-		public IEnumerable<Material> AvailableTradeItems
-		{
-			get
-			{
-				var array = new[]
+        /// <summary>
+        /// Elérhető termékek, amik még nincsenek meghirdetve
+        /// </summary>
+        public IEnumerable<Material> AvailableTradeItems
+        {
+            get
+            {
+                var array = new[]
 					       {
 						       Material.Wool, Material.Iron, Material.Clay, Material.Wood, Material.Wheat
 					       }.Except(Player.TradeItems.Keys.ToArray()).ToArray();
-				return array;
-			}
-		}
+                return array;
+            }
+        }
 
-		/// <summary>
-		/// Kereskedelmi termékek
-		/// </summary>
-		public IEnumerable<TradeItemContext> MyTradeItems
-		{
-			get { return Player.TradeItems.Values.Select(item => new TradeItemContext(this, item)).ToArray(); }
-		}
+        /// <summary>
+        /// Kereskedelmi termékek
+        /// </summary>
+        public IEnumerable<TradeItemContext> MyTradeItems
+        {
+            get { return Player.TradeItems.Values.Select(item => new TradeItemContext(this, item)).ToArray(); }
+        }
 
-		public IEnumerable<TradeItemContext> TradeItems
-		{
-			get
-			{
-				var items = new List<TradeItem>();
-				foreach (var player in GameTableContext.Players)
-				{
-					if (player != null && player != GameTableContext.CurrentPlayer)
-						items.AddRange(player.TradeItems.Values);
-				}
-				return items.Where(item => item.Quantity > 0)
-							.Select(item => new TradeItemContext(this, item))
-							.ToArray();
-			}
-		}
+        public IEnumerable<TradeItemContext> TradeItems
+        {
+            get
+            {
+                var items = new List<TradeItem>();
+                foreach (var player in GameTableContext.Players) {
+                    if (player != null && player != GameTableContext.CurrentPlayer)
+                        items.AddRange(player.TradeItems.Values);
+                }
+                return items.Where(item => item.Quantity > 0)
+                            .Select(item => new TradeItemContext(this, item))
+                            .ToArray();
+            }
+        }
 
-		/// <summary>
-		/// Kereskedelmi termék törlése
-		/// </summary>
-		public DelegateCommand<Material> DeleteCommand
-		{
-			get
-			{
-				return Lazy.Init(ref _DeleteCommand,
-					() => new DelegateCommand<Material>(
-						material =>
-						{
-							if (Player.TradeItems.ContainsKey(material))
-							{
-								Player.TradeItems.Remove(material);
-								OnPropertyChanged(() => MyTradeItems, () => AvailableTradeItems);
-							}
-						},
-						_ => Player != null));
-			}
-		}
+        /// <summary>
+        /// Kereskedelmi termék törlése
+        /// </summary>
+        public DelegateCommand<Material> DeleteCommand
+        {
+            get
+            {
+                return Lazy.Init(ref _DeleteCommand,
+                    () => new DelegateCommand<Material>(
+                        material => {
+                            if (Player.TradeItems.ContainsKey(material)) {
+                                Player.TradeItems.Remove(material);
+                                OnPropertyChanged(() => MyTradeItems, () => AvailableTradeItems);
+                            }
+                        },
+                        _ => Player != null));
+            }
+        }
 
-		/// <summary>
-		/// Új kereskedelmi termék hozzáadása
-		/// </summary>
-		public DelegateCommand<object> AddCommand
-		{
-			get
-			{
-				return Lazy.Init(ref _AddCommand,
-					() => new DelegateCommand<object>(
-								material =>
-								{
-									if (Player != null)
-									{
-										if (!Player.AddTradeItem(1, 1, (Material)material))
-											GameTableContext.WindowService.ShowMessageBox("Nincs ilyen nyersanyagod raktáron!", "Kevés a nyersanyag");
-										
-										Player.Materials[(Material)material]--;
-									}
-									OnPropertyChanged(() => AvailableTradeItems, () => MyTradeItems);
-								},
-								material => material is Material && AvailableTradeItems.Count() != 0
-							));
-			}
-		}
+        /// <summary>
+        /// Új kereskedelmi termék hozzáadása
+        /// </summary>
+        public DelegateCommand<object> AddCommand
+        {
+            get
+            {
+                return Lazy.Init(ref _AddCommand,
+                    () => new DelegateCommand<object>(
+                                material => {
+                                    if (Player != null) {
+                                        if (!Player.AddTradeItem(1, 1, (Material)material))
+                                            GameTableContext.WindowService.ShowMessageBox("Nincs ilyen nyersanyagod raktáron!", "Kevés a nyersanyag");
 
-		public override void Refresh()
-		{
-			base.Refresh();
-			if (GameTableContext != null)
-				GameTableContext.Refresh();
-		}
-	}
+                                        Player.Materials[(Material)material]--;
+                                    }
+                                    OnPropertyChanged(() => AvailableTradeItems, () => MyTradeItems);
+                                },
+                                material => material is Material && AvailableTradeItems.Count() != 0
+                            ));
+            }
+        }
+
+        public override void Refresh()
+        {
+            base.Refresh();
+            if (GameTableContext != null)
+                GameTableContext.Refresh();
+        }
+    }
 }
